750d3a7aba5cb0946bbe457f972cfdb0a4b7d307
Amrhub <amr_ahmed65@hotmail.com>
2021-10-20 00:28:58 +0200

    feat: using class instead of object

--- scripts/domManipulation.js
+++ scripts/domManipulation.js
@@ -1,7 +1,5 @@
 /* eslint-disable comma-dangle */
 /* eslint-disable prefer-const */
-const booksLocalStorage = JSON.parse(localStorage.getItem('books') || '[]');
-let books = booksLocalStorage;
 const booksContainer = document.querySelector('.books-container');
 
 function displayBooks() {
@@ -13,7 +11,7 @@ function displayBooks() {
     );
   }
 
-  books.forEach((book) => {
+  allBooks.books.forEach((book) => {
     const bookContainer = document.createElement('div');
     const bookTitle = document.createElement('p');
     const bookAuthor = document.createElement('p');
@@ -32,7 +30,3 @@ function displayBooks() {
     booksContainer.appendChild(bookContainer);
   });
 }
-
-// call the function on load for first time
-
-if (books.length) displayBooks();
