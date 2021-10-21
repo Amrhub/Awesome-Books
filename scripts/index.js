76d128da5c0cfe4ef3cbd02f9c6b884ec1a76353
Amrhub <amr_ahmed65@hotmail.com>
2021-10-21 19:41:13 +0200

    feat: add navigation to awesome books

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,15 @@
 /* eslint-disable no-use-before-define */
 /* eslint-disable object-shorthand */
 /* eslint-disable no-undef */
+
 const booksLocalStorage = JSON.parse(localStorage.getItem('books') || '[]');
 const form = document.getElementById('add-book');
+const booksList = booksContainer.parentElement;
+const addBookWrapper = form.parentElement;
+const contactUs = document.querySelector('.contact-us');
+const navLinks = Array.from(document.querySelectorAll('.navbar a'));
 let removeBtns;
+
 class allBooks {
   static books = booksLocalStorage;
 
@@ -51,6 +57,38 @@ function assignRemoveEventListener() {
 }
 
 // call the functions on load for first time
-
 displayBooks();
 assignRemoveEventListener();
+booksList.style.display = 'block';
+addBookWrapper.style.display = 'none';
+contactUs.style.display = 'none';
+
+function navigate(index) {
+  document.querySelector('.active-link').classList.remove('active-link');
+  switch (index) {
+    case 0:
+    case 1:
+      booksList.style.display = 'block';
+      addBookWrapper.style.display = 'none';
+      contactUs.style.display = 'none';
+      navLinks[1].classList.add('active-link');
+      break;
+
+    case 2:
+      addBookWrapper.style.display = 'block';
+      booksList.style.display = 'none';
+      contactUs.style.display = 'none';
+      navLinks[2].classList.add('active-link');
+      break;
+
+    default:
+      contactUs.style.display = 'flex';
+      addBookWrapper.style.display = 'none';
+      booksList.style.display = 'none';
+      navLinks[3].classList.add('active-link');
+  }
+}
+
+navLinks.forEach((navLink, index) => {
+  navLink.addEventListener('click', () => navigate(index));
+});
